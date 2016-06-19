8dd2ec54510672350943c300e4c0234f8bf0f7e8
xavier.lis <xavier.lis@gmail.com>
2016-06-19 22:15:04 +0300

    Add Data transformer for People Type

--- src/AppBundle/Form/PeopleType.php
+++ src/AppBundle/Form/PeopleType.php
@@ -3,18 +3,21 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+use \BW\Vkontakte as Vk;
+
 
 class PeopleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $vk = new Vk([
+            'client_id' => '5513448',
+            'client_secret' => 'ldSBVOfJVgP7VIRpcdEq',
+        ]);
         $builder
             ->add('name', TextType::class, array(
                 'required' => false,
@@ -27,7 +30,7 @@ class PeopleType extends AbstractType
                 'required' => true,
                 'label' => false,
                 'attr' => array(
-                    'placeholder' => 'Ссылка нв VK'
+                    'placeholder' => 'Ссылка на VK'
                 ),
             ))
             ->add('role', null, array(
@@ -36,6 +39,22 @@ class PeopleType extends AbstractType
                 'attr' => array(
                     'placeholder' => 'Роль'
                 ),
+            ))
+        ;
+        $builder->get('vkId')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($id) {
+                    // transform the array to a string
+                    return $id;
+                },
+                function ($idAsLink) use ($vk) {
+                    // transform the string back to an array
+                    $id = str_replace('http://vk.com/', '', $idAsLink);
+                    $user = $vk->api('users.get', [
+                        'user_ids' => $id,
+                    ]);
+                    return $user[0]['id'];
+                }
             ))
         ;
 
